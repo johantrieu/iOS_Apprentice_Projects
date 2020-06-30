eb7d885516a71bd46df1728a35e9a8d867aa74f1
johantrieu <johantrieu@gmail.com>
2020-07-01 00:16:05 +0300

    chapter 3

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -9,14 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Properties
+    // ==========
+    // User interface views
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
+    
+    // User interface content and layout
     
     var body: some View {
         VStack {
-            Text("Welcome to my first app!")
-                .fontWeight(.black)
-                .foregroundColor(.green)
-                
+            Spacer()
+            
+            // Target row
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("100")
+            }
+            
+            Spacer()
+            
+            // Slider row
+            
+            HStack {
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            
+            Spacer()
+            // TODO: Add views for the slider row here.
+            
+            // Button row
             
             Button(action: {
                 print("Button pressed!")
@@ -26,15 +51,42 @@ struct ContentView: View {
             }
             .alert(isPresented: self.$alertIsVisible) {
                 Alert(title: Text("Hello there!"),
-                  message: Text("This is my first pop-up."),
+                  message: Text("The slider's value is \(Int(sliderValue.rounded()))."),
                   dismissButton: .default(Text("Awesome!")))
             }
+            Spacer()
+            // Score row
+            HStack {
+                Button(action: {}) {
+                    Text("Start over")
+              }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {}) {
+                Text("Info")
+              }
+            }
+            .padding(.bottom, 20)
+            // TODO: Add views for the score, rounds, and start and info buttons here.
         }
     }
+    
+    // Methods
+    // =======
 }
 
+// Preview
+// =======
+
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
