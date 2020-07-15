e79831532d140a36019c6e94bfc969eeb4223ff2
johantrieu <johantrieu@gmail.com>
2020-07-15 10:19:52 +0300

    15 UIKit

---
+++ Bullseye2/Bullseye2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Bullseye2
+//
+//  Created by zalukana on 14.7.2020.
+//  Copyright © 2020 zalukana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: "This is my first app!",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        present(alert,animated: true, completion: nil)
+    }
+}
+
